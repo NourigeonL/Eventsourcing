85a5f2e9624e6195758d9ff87a5a0611b0897170
Louis <louis.nourigeon@gmail.com>
2024-07-05 05:20:05 +0900

    raise an TypeError instead of returning object

--- eventsourcing/data.py
+++ eventsourcing/data.py
@@ -2,6 +2,19 @@ from dataclasses import dataclass, is_dataclass
 from typing import get_args, get_origin, TypeVar
 
 def __get_subclass(cls : type, args : tuple, origin : type, key : str, value : any) -> type:
+    """
+    Helper function to determine the subclass type for nested structures.
+
+    Args:
+        cls: The parent class.
+        args: Type arguments of the parent class.
+        origin: Origin type of the parent class.
+        key: The key in the dictionary.
+        value: The value associated with the key.
+
+    Returns:
+        The determined subclass type.
+    """
     if is_dataclass(cls):
         sub_cls = cls.__dict__["__dataclass_fields__"][key].type
     elif origin == dict:
@@ -11,58 +24,75 @@ def __get_subclass(cls : type, args : tuple, origin : type, key : str, value : a
     return sub_cls
 
 T = TypeVar("T")
-def to_dict(obj : object) -> dict | object:
-        try:
-            res = {}
-            items_dict = {}
-            if isinstance(obj, dict):
-                items_dict = obj
-            elif is_dataclass(obj):
-                items_dict = vars(obj)
-            else:
-                return obj
-            for key, value in items_dict.items():
-                if isinstance(value, Data):
-                    res[key] = value.to_dict()
-                elif is_dataclass(value) or isinstance(value, dict):
-                    res[key] = to_dict(value)
-                elif isinstance(value, list):
-                    new_list = []
-                    for val in value:
-                        new_list.append(to_dict(val))
-                    res[key] = new_list
-                else:
-                    res[key] = value
-            return res
-        except Exception as e:
-            raise e
+
+def to_dict(obj : T|dict[str, any]) -> dict | object:
+    """
+    Convert an object or dictionary to a dictionary representation.
+
+    Args:
+        obj: The object or dictionary to convert.
+
+    Returns:
+        A dictionary representation of the input object.
+
+    Raises:
+        TypeError: If the input is neither a dictionary nor a dataclass.
+    """
+    res = {}
+    items_dict = {}
+    if isinstance(obj, dict):
+        items_dict = obj
+    elif is_dataclass(obj):
+        items_dict = vars(obj)
+    else:
+        raise TypeError("The object should be a dictionary or a dataclass")
+    for key, value in items_dict.items():
+        if isinstance(value, Data):
+            res[key] = value.to_dict()
+        elif is_dataclass(value) or isinstance(value, dict):
+            res[key] = to_dict(value)
+        elif isinstance(value, list):
+            new_list = []
+            for val in value:
+                new_list.append(to_dict(val))
+            res[key] = new_list
+        else:
+            res[key] = value
+    return res
 
 def from_dict(cls : type[T], values : any) -> T:
-    try:
-        if not isinstance(values, dict):
-            return cls(values)
-        new_dict = {}
-        args = get_args(cls)
-        origin = get_origin(cls)
-        for key, value in values.items():
-            if isinstance(value, dict):
-                sub_cls = __get_subclass(cls, args, origin, key, value)
-                if issubclass(sub_cls, Data):
-                    new_dict[key] = sub_cls.from_dict(value)
-                else:
-                    new_dict[key] = from_dict(sub_cls, value)
-            elif isinstance(value, list):
-                sub_cls = __get_subclass(cls, args, origin, key, value)
-                sub_arg = get_args(sub_cls)
-                new_list = []
-                for val in value:
-                    new_list.append(from_dict(sub_arg[0], val))
-                new_dict[key] = new_list
+    """
+    Convert a dictionary to an instance of the specified class.
+
+    Args:
+        cls: The target class type.
+        values: The dictionary or value to convert.
+
+    Returns:
+        An instance of the specified class.
+    """
+    if not isinstance(values, dict):
+        return cls(values)
+    new_dict = {}
+    args = get_args(cls)
+    origin = get_origin(cls)
+    for key, value in values.items():
+        if isinstance(value, dict):
+            sub_cls = __get_subclass(cls, args, origin, key, value)
+            if issubclass(sub_cls, Data):
+                new_dict[key] = sub_cls.from_dict(value)
             else:
-                new_dict[key] = value
-        return cls(**new_dict)
-    except Exception as e:
-        raise e
+                new_dict[key] = from_dict(sub_cls, value)
+        elif isinstance(value, list):
+            sub_cls = __get_subclass(cls, args, origin, key, value)
+            sub_arg = get_args(sub_cls)
+            new_list = []
+            for val in value:
+                new_list.append(from_dict(sub_arg[0], val))
+            new_dict[key] = new_list
+        else:
+            new_dict[key] = value
+    return cls(**new_dict)
 
 
 @dataclass
